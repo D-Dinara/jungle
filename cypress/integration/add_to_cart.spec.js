be8f6eb1b0a0b11f8ce7cb3a07306dfb7f2e02f2
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-04-19 14:07:43 -0400

    feature/testing-add-to-cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,4 +1,4 @@
-describe('home page', () => {
+describe('add_to_cart', () => {
   beforeEach(() => {
     cy.visit('/')
   })
@@ -6,8 +6,14 @@ describe('home page', () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
-  
+
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
   });
+
+  it('can add product to cart by clicking the button', () => {
+    cy.contains('My Cart (0)')
+    cy.get(".products article button").first().click({force: true})
+    cy.contains('My Cart (1)')
+  })
 }) 
